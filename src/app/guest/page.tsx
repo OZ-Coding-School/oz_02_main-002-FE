7e51ad3378ee361d7e03d57168aace27020a457e
surra7 <subum6302@gmail.com>
2024-06-06 16:41:51 +0900

    Styles: guestLayout 추가

--- src/app/guest/page.tsx
+++ src/app/guest/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 import GuestListItem from '@/components/GuestListItem';
-import NavBottom from '@/components/NavBottom';
+import { useRouter } from 'next/navigation';
 import { FaUserFriends } from 'react-icons/fa';
 
 export default function Guest() {
+  const router = useRouter();
   let guestBookList = [
     {
       name: '방명록1',
@@ -48,31 +49,30 @@ export default function Guest() {
     },
   ];
   return (
-    <main className="w-full h-full">
-      <section className="wrap-section relative">
-        <div className="w-full h-full bg-saturdayBlue absolute">배경</div>
-        <div className="w-full h-[calc(100%-2.6875rem)] absolute z-10 pt-11 px-[1.4375rem]">
-          <button className="h-[3.3125rem] bg-white bg-opacity-70 rounded-[0.625rem] font-semibold flex items-center ml-auto px-[0.625rem]">
-            친구 방명록 놀러가기
-            <span className="ml-2">
-              <FaUserFriends size={32} />
-            </span>
-          </button>
-          <div className="w-[21.5rem] h-[34.6875rem] rounded-[5px] bg-white border border-black-200 mt-[1.375rem] overflow-auto scroll-bar">
-            <ul className="py-2">
-              {guestBookList.map((item, index) => {
-                return (
-                  <li key={index} className="border-b-[0.5px] border-black-200">
-                    <GuestListItem item={item} />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+    <section className="w-full h-full relative">
+      <div className="w-full h-full bg-saturdayBlue absolute">배경</div>
+      <div className="w-full h-[calc(100%-2.6875rem)] absolute z-10 pt-11 px-[1.4375rem]">
+        <button
+          className="h-[3.3125rem] bg-white bg-opacity-70 rounded-[0.625rem] font-semibold flex items-center ml-auto px-[0.625rem]"
+          onClick={() => router.push('/guest/friends')}>
+          친구 방명록 놀러가기
+          <span className="ml-2">
+            <FaUserFriends size={32} />
+          </span>
+        </button>
+        <div className="w-[21.5rem] h-[34.6875rem] rounded-[5px] bg-white border border-black-200 mt-[1.375rem] overflow-auto scroll-bar">
+          <ul className="py-2">
+            {guestBookList.map((item, index) => {
+              return (
+                <li key={index} className="border-b-[0.5px] border-black-200">
+                  <GuestListItem item={item} />
+                </li>
+              );
+            })}
+          </ul>
         </div>
-        <div className="w-full h-[2.6875rem] border-y border-black-200 bg-white absolute bottom-0">공통 컴포넌트</div>
-      </section>
-      <NavBottom />
-    </main>
+      </div>
+      <div className="w-full h-[2.6875rem] border-y border-black-200 bg-white absolute bottom-0">공통 컴포넌트</div>
+    </section>
   );
 }
