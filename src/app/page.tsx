c58023e3d34600ee1a784d362e9d98142ac48be2
eoekstpal <park.se.mi47@gmail.com>
2024-06-12 15:09:51 +0900

    api 연동테스트 indefined 오류코드

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import NavBottom from '@/components/NavBottom';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BiCloset } from 'react-icons/bi';
 import { BsBox2Heart } from 'react-icons/bs';
 import { BiBowlRice } from 'react-icons/bi';
@@ -10,24 +10,34 @@ import { BiDonateHeart } from 'react-icons/bi';
 import { RiContactsBook2Line } from 'react-icons/ri';
 import MainPetButton from '@/components/main/MainPetButton';
 import PetStateMessage from '@/components/main/PetStateMessage';
+import PetProfile from '@/components/main/PetProfile';
+import axios from 'axios';
+import { petType } from '@/types/petType';
 
 function Main() {
+  const [petData, setPetData] = useState<petType>();
+  const [backgroundImageURL, setBackgroundImageURL] = useState('');
+
+  useEffect(() => {
+    axios.get<petType>('https://api.oz-02-main-04.xyz/api/v1/pets/mypet/1/')
+    .then(response => {
+      setPetData(response.data);
+      const backgroundImageURL = response.data.primary_background.item_name;
+      setBackgroundImageURL(backgroundImageURL);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  })
+
   return (
     <div className="w-full h-full">
-      <div className="wrap-section bg-[url(/background/spring.jpg)] bg-cover">
+      {/* bg-[url(/background/spring.jpg)] */}
+      <div className="wrap-section bg-cover" style={{ backgroundImage: `url:(${backgroundImageURL})`}}>
         <header className="h-1/6 pt-8 pb-2 bg-white">
-          <div className="w-5/6 pt-4 pb-2 mx-auto flex">
-            <p className="w-1/2 text-lg font-medium text-primary-600 text-start">수상한 알</p>
-            <div className="w-1/2 flex justify-end items-end">
-              <p className="px-1">1 Lv</p>
-              <p className="text-sm text-black-300">(30/50)</p>
-            </div>
-          </div>
-          <div>
-            <div className="w-5/6 h-4 rounded-3xl bg-black-100 mx-auto">
-              <div className="w-1/2 h-4 rounded-3xl bg-primary-500"></div>
-            </div>
-          </div>
+
+            <PetProfile name={petData?.primary_pet.pet_name} level={petData?.pet_rating} progress={petData.point} maxProgress={100} />
+
         </header>
 
         <main className="w-full h-5/6 ">
@@ -35,7 +45,7 @@ function Main() {
             <MainPetButton icon={<BiCloset size="30" />} label="보관함" />
             <div className="">
               <MainPetButton icon={<BsBox2Heart size="28" />} label="랜덤박스" />
-              <div className=" text-sm font-bold">1개 남음</div>
+              <div className=" text-sm font-bold">{petData?.random_boxes}개 남음</div>
             </div>
           </section>
 
@@ -55,7 +65,7 @@ function Main() {
                 <div className="text-sm font-bold">1개 남음</div>
               </div>
               <MainPetButton icon={<BiDonateHeart size="30" />} label="쓰다듬기" />
-              <MainPetButton icon={<RiContactsBook2Line size="30" />} label="방명록" link='/guest'/>
+              <MainPetButton icon={<RiContactsBook2Line size="30" />} label="방명록" link="/guest" />
             </div>
           </section>
         </main>
---
+++ src/types/petType.ts
@@ -0,0 +1,16 @@
+export interface petType {
+  user: number;
+  point: number;
+  hunger_degree: string;
+  random_boxes: number,
+  pet_rating: number,
+  primary_accessory: null;
+  primary_background: {
+      item_name: string;
+      image: string;
+  },
+  primary_pet: {
+      pet_name: string;
+      image: string;
+  }
+}
