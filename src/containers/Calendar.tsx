1cc8f148d8388be1ce8bbfc76cc8bc8efe812993
surra7 <subum6302@gmail.com>
2024-06-12 10:23:50 +0900

    Feat: 달력 투두데이트 get하기

--- src/containers/Calendar.tsx
+++ src/containers/Calendar.tsx
@@ -5,6 +5,10 @@ import { DAY_OF_WEEK, MONTH_OF_YEAR } from '@/constants';
 import useCreateCalendar from '@/hooks/useCreateCalendar';
 import useSwipeDirection from '@/hooks/useSwipeDirection';
 import { useEffect, useState } from 'react';
+import { getPostsList } from '../services/getPostsList';
+import { useAtomValue } from 'jotai';
+import { userAtom } from '@/atoms/atoms';
+import { postType } from '@/types/PostType';
 
 export default function Calendar() {
   const today = new Date().getDate();
@@ -13,29 +17,108 @@ export default function Calendar() {
   const [currentYear, setCurrentYear] = useState(todayYear);
   const [currentMonth, setCurrentMonth] = useState(todayMonth);
   const [calendar, setCalendar] = useState<string[][]>([]);
-  const possibleYear = ['2023년', '2024년'];
-  const possibleMonth = MONTH_OF_YEAR;
+  const [postsList, setPostsList] = useState<postType[]>([]);
+  const [startDate, setStartDate] = useState('');
+  const [lastDate, setLastDate] = useState('');
+  const [possibleYear, setPossibleYear] = useState<number[]>([]);
+  const [possibleMonth, setPossibleMonth] = useState<number[]>([]);
   const { direction, setDirection } = useSwipeDirection();
+  const user = useAtomValue(userAtom);
+
+  useEffect(() => {
+    async function getPosts() {
+      const response = await getPostsList(user?.id);
+      setPostsList(response);
+      console.log(today);
+      if (response.length !== 0) {
+        setStartDate(response[0].todo_date.slice(0, 7));
+        setLastDate(response[response.length - 1].todo_date.slice(0, 7));
+      } else {
+        setStartDate(todayYear.toString() + todayMonth.toString().padStart(2, '0'));
+        setLastDate(todayYear.toString() + todayMonth.toString().padStart(2, '0'));
+      }
+      // const startYear = Number(response[0].todo_date.slice(0, 4));
+      // const lastYear = Number(response[response.length - 1].todo_date.slice(0, 4));
+      // const startMonth = Number(response[0].todo_date.slice(5, 7));
+      // console.log('start month', startMonth);
+      // const lastMonth = Number(response[response.length - 1].todo_date.substring(5, 7));
+      // console.log('last month', lastMonth);
+      // let possibleYearList = [];
+      // let possibleMonthList = [];
+      // if (possibleYear[0] !== startYear) {
+      //   possibleYearList.push(startYear);
+      // }
+      // if (possibleMonth[0] !== startMonth) {
+      //   possibleMonthList.push(startMonth);
+      // }
+      // if (startYear !== lastYear) {
+      //   possibleYearList.push(lastYear);
+      // }
+      // if (startMonth !== lastMonth) {
+      //   possibleMonthList.push(lastMonth);
+      // }
+      // setPossibleYear(possibleYearList);
+      // setPossibleMonth(possibleMonthList);
+    }
+    getPosts();
+  }, []);
+
+  useEffect(() => {
+    const startYear = Number(startDate.slice(0, 4));
+    const lastYear = Number(lastDate.slice(0, 4));
+    const startMonth = Number(startDate.slice(5, 7));
+    const lastMonth = Number(lastDate.slice(5, 7));
+    if (currentYear === startYear && startYear !== lastYear) {
+      const startMonth = Number(startDate.slice(5, 7));
+      const lastMonth = 12;
+    } else if (currentYear === lastYear) {
+      const startMonth = 1;
+      const lastMonth = Number(lastDate.slice(5, 7));
+    } else if (currentYear > startYear && currentYear < lastYear) {
+      const startMonth = 1;
+      const lastMonth = 12;
+    }
+    if (possibleYear[0] !== startYear) {
+      setPossibleYear([...possibleYear, startYear]);
+    }
+    if (possibleMonth[0] !== startMonth) {
+      setPossibleMonth([...possibleMonth, startMonth]);
+    }
+    if (startYear !== lastYear) {
+      setPossibleYear([...possibleYear, lastYear]);
+    }
+    if (startMonth !== lastMonth) {
+      setPossibleMonth([...possibleMonth, lastMonth]);
+    }
+    postsList.map(post => {
+      console.log(post.todo_date);
+    });
+    console.log(possibleYear, possibleMonth);
+  }, [startDate, lastDate]);
 
   useEffect(() => {
     if (direction === 'left') {
-      setCurrentMonth(prev => prev + 1);
+      console.log(currentMonth, possibleMonth[possibleMonth.length - 1]);
+      if (currentYear === Number(startDate.slice(0, 4)) && currentMonth + 1 < possibleMonth[possibleMonth.length - 1])
+        return;
+      else setCurrentMonth(prev => prev + 1);
     } else if (direction === 'right') {
-      setCurrentMonth(prev => prev - 1);
+      if (currentMonth - 1 < possibleMonth[0]) return;
+      else setCurrentMonth(prev => prev - 1);
     }
     setDirection('');
-  }, [direction]);
+  }, [direction, possibleMonth]);
 
   useEffect(() => {
     if (currentMonth < 1) {
-      if (currentYear === 2023) {
+      if (currentYear - 1 < possibleYear[0]) {
         setCurrentMonth(prev => prev + 1);
         return;
       }
       setCurrentYear(prev => prev - 1);
       setCurrentMonth(12);
     } else if (currentMonth > 12) {
-      if (currentYear === 2024) {
+      if (currentYear + 1 > possibleYear[possibleYear.length - 1]) {
         setCurrentMonth(prev => prev - 1);
         return;
       }
@@ -43,7 +126,7 @@ export default function Calendar() {
       setCurrentMonth(1);
     }
     setCalendar(() => useCreateCalendar(currentYear, currentMonth));
-  }, [currentYear, currentMonth]);
+  }, [currentYear, currentMonth, possibleYear]);
 
   return (
     <div className="w-full">
@@ -86,16 +169,18 @@ export default function Calendar() {
                         <td
                           key={i}
                           className={`w-[3.1875rem] h-full text-xs ${i !== 0 && i !== 6 && 'border-[0.0313rem]'} align-middle text-center border-black-200 `}>
-                          <div
-                            className={`w-[1.125rem] h-[1.125rem] rounded-full flex justify-center items-center ${today === Number(day) && todayMonth === currentMonth && todayYear === currentYear ? 'bg-primary-500 text-white' : ''} mx-auto`}>
-                            {day}
-                          </div>
-                          {day !== '' && (
-                            <div className="w-full mx-auto mt-1">
-                              <div className="h-3 flex justify-center items-center text-2xs">87%</div>
-                              <ProgressBar rate={87} />
+                          <button>
+                            <div
+                              className={`w-[1.125rem] h-[1.125rem] rounded-full flex justify-center items-center ${today === Number(day) && todayMonth === currentMonth && todayYear === currentYear ? 'bg-primary-500 text-white' : ''} mx-auto`}>
+                              {day}
                             </div>
-                          )}
+                            {day !== '' && (
+                              <div className="w-full mx-auto mt-1">
+                                <div className="h-3 flex justify-center items-center text-2xs">87%</div>
+                                <ProgressBar rate={87} />
+                              </div>
+                            )}
+                          </button>
                         </td>
                       );
                     })}
