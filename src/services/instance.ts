a1650085497d1731e9b5fea7bbccfc8b14814255
surra7 <subum6302@gmail.com>
2024-06-18 10:31:16 +0900

    Fix: 토큰 로직 수정

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -1,15 +1,18 @@
 import { accessTokenAtom, csrfTokenAtom } from '@/atoms/atoms';
 import _axios, { AxiosError } from 'axios';
 import { useAtom } from 'jotai';
-import { useEffect } from 'react';
-
-// useEffect(() => {
-//   const [accessToken] = useAtom(accessTokenAtom);
-//   const [csrfToken] = useAtom(csrfTokenAtom);
-// }, [third]);
 
 _axios.defaults.xsrfCookieName = 'csrftoken';
 _axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
+
+const useSetAccessToken = () => {
+  const [accessToken] = useAtom(accessTokenAtom);
+  return accessToken;
+};
+const useSetCSRFToken = () => {
+  const [csrfToken] = useAtom(csrfTokenAtom);
+  return csrfToken;
+};
 
 export const axios = _axios.create({
   baseURL: 'https://api.oz-02-main-04.xyz/api/v1/',
@@ -25,11 +28,11 @@ export const axios = _axios.create({
 export const setupAxiosInterceptors = () => {
   axios.interceptors.request.use(
     config => {
-      const [accessToken] = useAtom(accessTokenAtom);
-      const [csrfToken] = useAtom(csrfTokenAtom);
+      const accessToken = useSetAccessToken();
+      const csrfToken = useSetCSRFToken();
       if (accessToken && csrfToken) {
-        config.headers.Authorization = `Bearer ${useAtom(accessTokenAtom)}`;
-        config.headers['x-csrftoken'] = `${useAtom(csrfTokenAtom)}`;
+        config.headers.Authorization = `Bearer ${accessToken}`;
+        config.headers['x-csrftoken'] = `${csrfToken}`;
       }
       // config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE4NjY1Njg2LCJpYXQiOjE3MTg2Mjk2ODYsImp0aSI6Ijk2OTg4ZmFiN2E4MzRjNDI4OTMyM2RkNGJhM2NkYzdjIiwidXNlcl9pZCI6MTR9.GbVLLzbM-RcSfCXkpRBFbhvfh_BSM0aaiHGBX2H92dU`;
       return config;
